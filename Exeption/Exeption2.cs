38451a076fb11a2c3f8304e0d2b6c6be1ac1345f
Ani Mkrtchyan <mkrtchyan.ani@outlook.com>
2022-06-19 22:52:48 +0400

    IndexOutRangeException

---
+++ Exeption/Exeption2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Exeption2
+{
+    public class arrayoutofindex
+    {
+        public void calculatedifference()
+        {
+            int difference = 0;
+            int[] number = new int[5] { 1, 2, 3, 4, 5 };
+            try
+            {
+                for (int init = 1; init <= 5; init++)
+                {
+                    difference = difference - number[init];
+                }
+                Console.WriteLine("The difference of the array is:" + difference);
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+        class classmain
+        {
+            static void Main(string[] args)
+            {
+                arrayoutofindex obj = new arrayoutofindex();
+                obj.calculatedifference();
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
+
---
+++ Exeption/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+
+namespace Exeption
+{
+    //public class DivZero:Exception
+    //{
+    //    static public int IntResult(int num, int denom)
+    //    {
+    //        return (num / denom);
+    //    }
+    //    static public double DoubleResult(double num, double denom)
+    //    {
+    //        return (num / denom);
+    //    }
+    //    static public void Main()
+    //    {
+    //        // Set an integer equal to 0
+    //        int IntVal1 = int.Parse(Console.ReadLine()); ;
+    //        // and another not equal to zero
+    //        int IntVal2 = 53;
+    //        int result = IntVal2 / IntVal1;
+    //        try
+    //        {
+    //            Console.WriteLine("{0} / {1} = {2}", IntVal2, IntVal1, result);
+    //        }
+
+    //        catch (DivideByZeroException ex)
+    //        {
+    //            //   Console.WriteLine(ex.Message);
+    //        }
+    //        catch (InvalidOperationException ex)
+    //        {
+    //            Console.Write("Invalid operation. Please try again.");
+    //        }
+    //        catch (FormatException ex)
+    //        {
+    //            Console.Write("Not a valid format. Please try again.");
+    //        }
+
+    //        // Set a double equal to 0
+    //        double dVal1 = 0.0;
+    //        double dVal2 = 53.3;
+    //        try
+    //        {
+    //            Console.WriteLine("{0} / {1} = {2}", dVal2, dVal1, DoubleResult(dVal2, dVal1));
+    //        }
+    //        catch (DivideByZeroException e)
+    //        {
+    //            Console.WriteLine(e.Message);
+    //        }
+    //        catch (Exception e)
+    //        {
+    //            throw e;
+    //        }
+    //    }
+
+
+    //  }
+         
+        
+}
